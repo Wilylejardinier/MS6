2951d9951c5238a8053664a1e52d265c7210ade1
Wilylejardinier <wilylejardinier02@gmail.com>
2022-06-14 08:37:43 +0200

    premier commit

---
+++ echiquier.h
@@ -0,0 +1,19 @@
+//---------------------------------------------------------------------------
+
+#ifndef echiquierH
+#define echiquierH
+//---------------------------------------------------------------------------
+#endif
+
+
+
+class Echiquier
+{
+	private:
+ char [0..*] tabEchiquier;
+ int demiCoup;
+
+ public
+
+};
+
